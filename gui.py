818c5c64a44dfd591ce8fb453d9bc6e315c5daf7
Marco Antonio Islas <marco3islas@gmail.com>
2023-10-25 11:39:11 -0600

    Se agregaron los nuevos elementos al gui

--- gui.py
+++ gui.py
@@ -1,11 +1,12 @@
 import functions
 import PySimpleGUI as sg
 
-label = sg.Text("Type in a to-do")
+label = sg.Text("Type in a to-do: ")
 input_box = sg.InputText(tooltip="Enter todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", tooltip="Add the todo")
+close_button = sg.CloseButton("x", tooltip="Close the window")
 
-window = sg.Window("Marco's TO-DO App", layout=[[label], [input_box, add_button]])
+window = sg.Window("Marco's TO-DO App", layout=[[label], [input_box, add_button],[close_button]])
 
 window.read()
 window.close()

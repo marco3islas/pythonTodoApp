04ed0b0e2e823649381d97e13fd62602ade3e26b
Marco Antonio Islas <marco3islas@gmail.com>
2023-10-31 21:14:30 -0600

    Se modifico el path de todos.txt para que quede en la raiz

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 
-FILEPATH = "files/todos.txt"
+FILEPATH = "todos.txt"
 
 
 def get_todos(filepath=FILEPATH): #Default parameter
     """
-    Read a text file and return the list
-    of todos items.
+        Read a text file and return the list
+        of todos items.
     """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
@@ -13,7 +13,8 @@ def get_todos(filepath=FILEPATH): #Default parameter
 
 
 
-def write_todos(todos_arg, filepath=FILEPATH ): #los default parameter deben ir despues de los no default parameter.
+def write_todos(todos_arg, filepath=FILEPATH ): # los default parameter deben
+    # ir despues de los no default parameter.
     """Write the todo items list in a file text."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
